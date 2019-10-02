093ef6a71f94f37e3f52f2f1ae515c97508d4b96
Wilson Neto <contato@wilsonneto.com.br>
2019-10-02 06:43:09 -0300

    added nodemon and npm configs

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send('Hello World');
+    return res.json({ message: 'Hello World ok 2' });
 })
 
 app.listen(3333);
